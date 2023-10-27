d261ff50ba8cbd963a9dd9757cf821dcc61cb3c0
phantxom <ahmedwasel183@gmail.com>
2023-10-27 12:29:06 +0200

    feat added anmitaion to 404 page

--- frontend/src/pics/NotFound.jsx
+++ frontend/src/pics/NotFound.jsx
@@ -6,10 +6,10 @@ import Container from "react-bootstrap/esm/Container"
 const NotFound = () => {
     return (
         <Container style={{color: "#00dba0", fontSize: "60px"}}>
-            <h1 className="mt-5 pt-5" style={{color: "#00dba0", fontSize: "60px"}}>404 Page NotFound</h1>
-            <div style={{display: "block"}}>
-                <span className="" style={{display: "block"}}>\_(-_-)_/</span>
-                <span style={{display: "block", height: "auto !important"}}>|<span className="m-2"></span>|</span>
+            <h1 className="mt-5 pt-5 not-found-txt" style={{color: "#00dba0", fontSize: "60px"}}>404 Page NotFound</h1>
+            <div className="not-found" style={{display: "block"}}>
+                <span className="not-found1" style={{display: "block"}}>\_(-_-)_/</span>
+                <span className="not-found2" style={{display: "block", height: "auto !important"}}>|<span className="m-2"></span>|</span>
             </div>
         </Container>
     )
