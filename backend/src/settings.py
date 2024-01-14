43ea826f918fdec558d597ed67829e4cae6bc162
mohamedhw <mohamedhw2000@gmail.com>
2024-01-14 06:19:30 +0200

    added static root

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -17,7 +17,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -163,7 +162,8 @@ EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False  # Set to True if you're using SSL
-DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL") # The email address from which emails will be sent
+# The email address from which emails will be sent
+DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
 
 PASSWORD_RESET_TIMEOUT = 14400
 
