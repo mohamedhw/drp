e1a1083336954b7b4b5f0a9fc1933232db350309
mohamedhw <mohamedhw2000@gmail.com>
2024-01-14 18:35:28 +0200

    fix: fix the api path

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -17,6 +17,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+
+
 # Application definition
 
 INSTALLED_APPS = [
