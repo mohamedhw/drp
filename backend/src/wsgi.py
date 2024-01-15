b72ebffeee04a6a8c3170460fcfc367106745512
Mohamed Wasel <68668789+mohamedhw@users.noreply.github.com>
2024-01-15 19:02:17 +0200

    Update wsgi.py

--- backend/src/wsgi.py
+++ backend/src/wsgi.py
@@ -1,8 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application)
