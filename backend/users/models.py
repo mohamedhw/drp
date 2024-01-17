23511e40ddf47ef6ac5b2ee1fb3bda7a7517fee6
mohamedhw <mohamedhw2000@gmail.com>
2024-01-17 02:32:47 +0200

    fix for the def pic profile

--- backend/users/models.py
+++ backend/users/models.py
@@ -5,10 +5,13 @@ from django.contrib.auth.models import User
 def upload_to(instance, filename):
     return 'images/{filename}'.format(filename=filename)
 
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(null=True, upload_to=upload_to, default='default.jpg')
-    cover = models.ImageField(null=True, upload_to='profile/cover', default='default.jpg')
+    image = models.ImageField(
+        null=True, upload_to=upload_to, default='default.jpg')
+    cover = models.ImageField(
+        null=True, upload_to='profile/cover', default='default.jpg')
 
     def user_u(self):
         return self.user.username
