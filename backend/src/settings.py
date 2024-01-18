ec74b30103d81a0b94cb1a1734b7aac0b9cc4e3f
Mohamed Wasel <68668789+mohamedhw@users.noreply.github.com>
2024-01-18 19:48:23 +0200

    Update settings.py

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -156,8 +156,8 @@ REST_FRAMEWORK = {
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_FROM = os.environ.get("DEFAULT_FROM_EMAIL")
-EMAIL_HOST_USER = os.environ.get("DEFAULT_FROM_EMAIL")
+EMAIL_FROM = os.environ.get("DEFAULT_EMAIL_FORM")
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
