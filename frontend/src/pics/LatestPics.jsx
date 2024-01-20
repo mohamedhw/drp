fdddb48138bb5a32edf344cd914e1ca0cb3e202f
mohamedhw <mohamedhw2000@gmail.com>
2024-01-21 00:03:09 +0200

    fix:added the filter bar to Home

--- frontend/src/pics/LatestPics.jsx
+++ frontend/src/pics/LatestPics.jsx
@@ -1,6 +1,6 @@
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 import { useState, useEffect } from 'react';
-import {pics} from '../redux/action/pics'
+import { pics } from '../redux/action/pics'
 import Container from 'react-bootstrap/Container';
 import Pagination from '../component/Pagination';
 import Items from '../component/Items';
@@ -8,31 +8,31 @@ import { useParams } from "react-router-dom"
 import { useNavigate } from 'react-router-dom';
 import { setPage, setCurrentPage } from '../redux/action/pages'; // Import your new actions
 
-const LatestPics = ({pics_g, pics, setCurrentPage, currentPage, count, next, previous}) => {
+const LatestPics = ({ pics_g, pics, setCurrentPage, currentPage, count, next, previous }) => {
     const navigate = useNavigate()
     const apiUrl = import.meta.env.VITE_API_URL;
 
     const { page } = useParams();
     const isPageProvided = Boolean(page);
 
-    
+
 
     let url = ``
     useEffect(() => {
         // setCurrentPage(page)
         if (currentPage != null) {
             url = isPageProvided
-            ? `${apiUrl}/api-post/?page=${currentPage}`
-            : `${apiUrl}/api-post/`;
+                ? `${apiUrl}/api-post/?page=${currentPage}`
+                : `${apiUrl}/api-post/`;
             navigate(`/latest/${currentPage}`);
-        }else if (currentPage === null & page > 1){
+        } else if (currentPage === null & page > 1) {
             url = `${apiUrl}/api-post/?page=${page}`;
             navigate(`/latest/${page}`);
-        }else {
+        } else {
             // navigate(`/`);
-            url = `${apiUrl}/api-post/`; 
+            url = `${apiUrl}/api-post/`;
         }
-        
+
         // if (currentPage !== 1) {
         //     navigate(`/${currentPage}`);
         // }
@@ -40,16 +40,16 @@ const LatestPics = ({pics_g, pics, setCurrentPage, currentPage, count, next, pre
         // console.log(url)
     }, [currentPage, page]);
 
-    return(
-        <Container className='mt-5'>
-            <h1 style={{float: "left", color: "#00bda0"}} className="mb-5">Latest Pics</h1>
-            {pics_g && 
+    return (
+        <div style={{ margin: "0 8%" }}>
+            <h1 style={{ float: "left", color: "#00bda0" }} className="mb-5">Latest Pics</h1>
+            {pics_g &&
                 <>
                     <Items pics_g={pics_g} />
-                    <Pagination  page={page} count={count} currentPage={currentPage} next={next} previous={previous}/>
+                    <Pagination page={page} count={count} currentPage={currentPage} next={next} previous={previous} />
                 </>
             }
-        </Container>
+        </div>
     )
 }
 
@@ -61,4 +61,4 @@ const mapStateToProps = state => ({
     next: state.pics.pics.next,
     previous: state.pics.pics.previous,
 })
-export default connect(mapStateToProps, {pics, setPage, setCurrentPage}) (LatestPics)
+export default connect(mapStateToProps, { pics, setPage, setCurrentPage })(LatestPics)
