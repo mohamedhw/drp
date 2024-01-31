9205efcc094f8a2fefcbe9040d3f41a98149a6c8
Mohamed Wasel <68668789+mohamedhw@users.noreply.github.com>
2024-01-31 21:51:18 +0200

    Update PicContent.jsx

--- frontend/src/component/PicContent.jsx
+++ frontend/src/component/PicContent.jsx
@@ -108,7 +108,7 @@ const PicContent = ({ data, zoom_, setZoom_, loading }) => {
             style={{
                 marginRight: "-16.8px",
                 marginBottom: "-16.8px",
-                height: '94vh',
+                height: '85vh',
                 display: 'flex',
                 alignItems: 'center',
                 scrollBehavior: 'smooth'
