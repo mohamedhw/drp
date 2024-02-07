58abd82ce1400fecaa284c1e5af7d7ca1754c09f
mohamedhw <mohamedhw2000@gmail.com>
2024-02-07 02:13:34 +0200

    updated the db

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -3,6 +3,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
